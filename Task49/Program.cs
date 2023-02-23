c3a56fb06d217d8c0721fa7fdf3de05f13cdfd79
Makc <m_stefanenkov@mail.ru>
2023-02-23 23:59:18 +0100

    Решили задачу 49

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -35,11 +35,11 @@ void PrintMatrix(int[,] matrix)
 }
 void FindEvenValues(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i+=2)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j+=2)
         {
-            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i, j];
+            matrix[i, j] *= matrix[i, j];
         }
     }
 }
